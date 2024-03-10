2ea2a21a706bea3ca294f126c85bfed68e676b8e
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-03-10 08:46:44 +0100

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,12 +10,12 @@ def pascal_triangle(n):
 
     triangle = []
     for i in range(n):
-        row = [1]  # first element of each row is always 1
+        row = [1]  
         if i > 0:
             prev_row = triangle[-1]
             for j in range(1, i):
                 row.append(prev_row[j - 1] + prev_row[j])
-            row.append(1)  # last element of each row is always 1
+            row.append(1)  
         triangle.append(row)
 
     return triangle
