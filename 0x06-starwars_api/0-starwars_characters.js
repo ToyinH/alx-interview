2e3c210b678f3a12855811f5e39c01a15c82e442
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-04-19 19:30:39 +0100

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,18 +3,18 @@
 const request = require('request');
 
 // Function to retrieve characters for a given movie ID
-function getMovieCharacters(movieId) {
+function getMovieCharacters (movieId) {
   return new Promise((resolve, reject) => {
     // Make a request to the Star Wars API to get the movie details
     const movieUrl = `https://swapi.dev/api/films/${movieId}/`;
     request.get(movieUrl, (error, response, body) => {
       if (error) {
-        reject(`Error fetching movie details: ${error}`);
+        reject(new Error(`Error fetching movie details: ${error}`));
         return;
       }
 
       if (response.statusCode !== 200) {
-        reject(`Failed to fetch movie details: ${response.statusCode}`);
+        reject(new Error(`Failed to fetch movie details: ${response.statusCode}`));
         return;
       }
 
@@ -27,12 +27,12 @@ function getMovieCharacters(movieId) {
         charactersPromises.push(new Promise((resolveCharacter, rejectCharacter) => {
           request.get(url, (errorCharacter, responseCharacter, bodyCharacter) => {
             if (errorCharacter) {
-              rejectCharacter(`Error fetching character details: ${errorCharacter}`);
+              rejectCharacter(new Error(`Error fetching character details: ${errorCharacter}`));
               return;
             }
 
             if (responseCharacter.statusCode !== 200) {
-              rejectCharacter(`Failed to fetch character details: ${responseCharacter.statusCode}`);
+              rejectCharacter(new Error(`Failed to fetch character details: ${responseCharacter.statusCode}`));
               return;
             }
 
@@ -54,7 +54,7 @@ function getMovieCharacters(movieId) {
 }
 
 // Main function
-function main() {
+function main () {
   if (process.argv.length !== 3) {
     console.error('Usage: ./script.js <movie_id>');
     process.exit(1);
