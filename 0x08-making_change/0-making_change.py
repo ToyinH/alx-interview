1c8b9f33901026941078def54c62ddb0ec57e953
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-05-01 14:38:28 +0100

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -27,26 +27,10 @@ def makeChange(coins, total):
     - The solution's runtime will be evaluated in this task.
     """
     if total <= 0:
-        #raise ValueError("Total must be greater than 0")
         return 0
 
     if total == 0:
         return 0
-
-    # # Create a list to store the minimum number of coins needed for each total
-    # dp = [float('inf')] * (total + 1)
-    # dp[0] = 0
-
-    # # Iterate through all possible totals up to 'total'
-    # for i in range(1, total + 1):
-    #     # Iterate through each coin value
-    #     for coin in coins:
-    #         if coin <= i:
-    #             # Update dp[i] if using the current coin
-    #             # results in a smaller number of coins
-    #             dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    # return dp[total] if dp[total] != float('inf') else -1
 
     # Sort coins in descending order
     coins.sort(reverse=True)
