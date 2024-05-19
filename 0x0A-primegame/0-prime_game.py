752cddb0fcaab857537b54d468ff9a780b9f9e3f
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-05-19 16:55:32 +0100

    update

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,42 +3,53 @@
 Prime Game
 """
 
-
-def is_prime(num):
+def sieve_of_eratosthenes(max_n):
     """
-    is_prime function
+    Generate a list of prime statuses for numbers up to max_n using the Sieve of Eratosthenes
     """
-    if num <= 1:
-        return False
-    if num <= 3:
-        return True
-    if num % 2 == 0 or num % 3 == 0:
-        return False
-    i = 5
-    while i * i <= num:
-        if num % i == 0 or num % (i + 2) == 0:
-            return False
-        i += 6
-    return True
+    is_prime = [True] * (max_n + 1)
+    is_prime[0] = is_prime[1] = False  # 0 and 1 are not primes
+    p = 2
+    while (p * p <= max_n):
+        if is_prime[p]:
+            for i in range(p * p, max_n + 1, p):
+                is_prime[i] = False
+        p += 1
+    return is_prime
 
+def count_primes_up_to(is_prime, max_n):
+    """
+    Count the number of primes up to each number from 0 to max_n
+    """
+    prime_count = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if is_prime[i] else 0)
+    return prime_count
 
 def isWinner(x, nums):
     """
-    isWinner function
+    Determine the winner after x rounds
     """
-
     if x <= 0 or not nums:
         return None
-
+    
+    max_n = max(nums)
+    is_prime = sieve_of_eratosthenes(max_n)
+    prime_count = count_primes_up_to(is_prime, max_n)
+    
     maria_wins = 0
     ben_wins = 0
+    
     for n in nums:
-        prime_count = sum(1 for i in range(1, n+1) if is_prime(i))
-        # If the count of prime numbers is even, Ben wins, otherwise Maria wins
-        if prime_count % 2 == 0:
+        if n == 0:
+            continue
+        
+        # Use precomputed prime counts
+        if prime_count[n] % 2 == 0:
             ben_wins += 1
         else:
             maria_wins += 1
+    
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
