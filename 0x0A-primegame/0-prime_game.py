65f78f1e1e4e7d91fa08ea3b4320b24c1302e569
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-05-19 16:57:15 +0100

    update

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,9 +3,11 @@
 Prime Game
 """
 
+
 def sieve_of_eratosthenes(max_n):
     """
-    Generate a list of prime statuses for numbers up to max_n using the Sieve of Eratosthenes
+    Generate a list of prime statuses for numbers
+    up to max_n using the Sieve of Eratosthenes
     """
     is_prime = [True] * (max_n + 1)
     is_prime[0] = is_prime[1] = False  # 0 and 1 are not primes
@@ -17,6 +19,7 @@ def sieve_of_eratosthenes(max_n):
         p += 1
     return is_prime
 
+
 def count_primes_up_to(is_prime, max_n):
     """
     Count the number of primes up to each number from 0 to max_n
@@ -26,30 +29,31 @@ def count_primes_up_to(is_prime, max_n):
         prime_count[i] = prime_count[i - 1] + (1 if is_prime[i] else 0)
     return prime_count
 
+
 def isWinner(x, nums):
     """
     Determine the winner after x rounds
     """
     if x <= 0 or not nums:
         return None
-    
+
     max_n = max(nums)
     is_prime = sieve_of_eratosthenes(max_n)
     prime_count = count_primes_up_to(is_prime, max_n)
-    
+
     maria_wins = 0
     ben_wins = 0
-    
+
     for n in nums:
         if n == 0:
             continue
-        
+
         # Use precomputed prime counts
         if prime_count[n] % 2 == 0:
             ben_wins += 1
         else:
             maria_wins += 1
-    
+
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
